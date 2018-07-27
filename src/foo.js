ad94e6bb0ab1651edf55f579c9e14a2846cc3dbd
xuanzebin <xuanzebin@126.com>
2018-07-27 23:03:34 +0800

    添加foo.js

---
+++ src/foo.js
@@ -0,0 +1,4 @@
+export default {hi:function(){
+    return '方方的教程'
+  },
+  animal:'cat'}
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import _ from 'lodash'
 import $ from 'jquery'
+import foo from './foo'
 function component() {
   var element = $('<div></div>');
 
@@ -9,3 +10,5 @@ function component() {
 }
 
 document.body.appendChild(component());
+console.log(foo.hi())
+console.log(foo.cat)
